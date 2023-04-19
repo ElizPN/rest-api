a243e33064bbebb0bcd3947bb24c26851a35f76a
ElizPN <pankevych.liza@gmail.com>
2023-04-19 17:36:14 +0200

    create form

--- client/frontend.js
+++ client/frontend.js
@@ -1,3 +1,15 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 console.log(createApp);
+
+createApp({
+  data() {
+    return {
+      form: {
+        name: "Liza",
+        value: "xs",
+      },
+      contacts: []
+    };
+  },
+}).mount("#app");
